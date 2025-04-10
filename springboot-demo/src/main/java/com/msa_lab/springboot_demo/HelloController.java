f85c9adfa8077ade681bd61f6424f4783a590d22
Pa-fs <qordi124@gmail.com>
2025-04-10 15:34:49 +0900

    add SonarQube setting

--- springboot-demo/src/main/java/com/msa_lab/springboot_demo/HelloController.java
+++ springboot-demo/src/main/java/com/msa_lab/springboot_demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.msa_lab.springboot_demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class HelloController {
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/")
     public String helloWorld() {
+//        logger.info("Hello world on kubernetes!!!");
+        System.out.println("Hello world on kubernetes!!!");
+
         return "🔑 Hello World on kubernetes!!!";
     }
 }
